e788ca0303e1c8ef746a13c2b4039784dd17bcf6
Tunastrom <36443273+Tunastrom@users.noreply.github.com>
2021-12-11 08:02:23 +0900

    21.12.11_FastAPI_study

---
+++ my_fastAPI/main_3.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+''' declare body parameters as optional
+@app.put("/items/{item_id}")
+async def updated_item(
+    *,
+    item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
+    q: Optional[str] = None,
+    item: Optional[Item] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results'''
+
+'''Multiple body parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results'''
+
+
+
