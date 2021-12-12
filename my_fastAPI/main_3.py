cd41f3b392b7b458431e16d41c106823e2390c68
Tunastrom <36443273+Tunastrom@users.noreply.github.com>
2021-12-12 18:51:16 +0900

    21.12.12_fastAPI_study

--- my_fastAPI/main_3.py
+++ my_fastAPI/main_3.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Body
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,7 +15,8 @@ class User(BaseModel):
     username: str
     full_name: Optional[str] = None
 
-''' declare body parameters as optional
+# declare body parameters as optional
+''' 
 @app.put("/items/{item_id}")
 async def updated_item(
     *,
@@ -30,11 +31,41 @@ async def updated_item(
         results.update({"item": item})
     return results'''
 
-'''Multiple body parameters
+# Multiple body parameters
+'''
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item, user: User):
     results = {"item_id": item_id, "item": item, "user": user}
     return results'''
 
+# Singular values in body 
+'''
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: int = Body(...)
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance }
+    return results'''
 
+# Multiple body params and query
+'''
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: int = Body(..., gt=0),
+    q: Optional[str] = None,
+):
+    results = {"item_id": item_id, "item": item, "user":user, "importance": importance}
+    if q:
+        results.update({"q": q})
+    return results'''
 
+# Embed a single body parameter
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
