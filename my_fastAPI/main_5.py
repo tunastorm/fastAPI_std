19cd9fff6dc7322c02fe2465827db462fa05addf
Tunastrom <36443273+Tunastrom@users.noreply.github.com>
2022-01-26 23:49:16 +0900

    umm~

--- my_fastAPI/main_5.py
+++ my_fastAPI/main_5.py
@@ -1,18 +1,40 @@
-from typing import Optional
+from typing import List, Optional, Set
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
+# Define a submodel
+class Image(BaseModel):
+    # special types and validation
+    url: HttpUrl
+    name: str
+
+# List fields
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
-    tags: list = []
-    
+    # tags: List[str] = []
+    tags: Set[str] = ()
+    # Use the submodel as a type
+    image: Optional[Image] = None
+
+'''    
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results'''
+
+# List fields with type parameter
+
+# Set types
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+
